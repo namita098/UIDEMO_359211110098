decd4b2080c90cb300c727c9d9f55d445ab14e23
namita098 <mind_roverden35@hotmail.com>
2018-02-25 20:45:06 +0700

    loginModel.java

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -5,14 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
-    private static final String SQCONN = "jdbc:school.sqlite";
-    public static Connection getConnection () throws SQLException {
+    private static final String SQCONN = "jdbc:sqlite:school.sqlite";
+
+    public static Connection getConnection() throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
             return DriverManager.getConnection(SQCONN);
-        } catch (ClassNotFoundException ex){
+        } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         }
         return null;
-    }
-}
+    }//getConnection
+
+}//class
